4df4ec43b7cebfd18d13beaa0d8c17ef92905acd
coffeedevr <122944372+coffeedevr@users.noreply.github.com>
2023-06-11 15:52:21 +0800

    Change date_created_formatted virtual schema to reflect duration of post since creation

--- models/thread_model.js
+++ models/thread_model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
-const { DateTime } = require("luxon");
+const { DateTime, Interval } = require("luxon");
 
 const ThreadSchema = new Schema({
     user: { type: String, maxLength: 18},
@@ -12,7 +12,22 @@ const ThreadSchema = new Schema({
 }, { collection: 'threads' })
 
 ThreadSchema.virtual("date_created_formatted").get(function() {
-  return DateTime.fromISO(this.date_created.toISOString()).toLocaleString(DateTime.DATETIME_MED)
+  // return DateTime.fromISO(this.date_created.toISOString()).toLocaleString(DateTime.DATETIME_MED)
+  const currentDate = DateTime.fromISO(new Date().toISOString())
+  const dateCreation = DateTime.fromISO(this.date_created.toISOString())
+  const res = Interval.fromDateTimes(dateCreation, currentDate)
+
+  return (
+    res.length('seconds') < 60 ?
+      Math.trunc(res.length('seconds')) + ' seconds ago' :
+    res.length('minutes') < 60 ?
+      Math.trunc(res.length('minutes')) + ' minutes ago' :
+    res.length('hours') < 24 ?
+      Math.trunc(res.length('hours')) + ' hours ago' :
+    res.length('days') < 7 ?
+      Math.trunc(res.length('days')) + ' days ago' :
+      Math.trunc(res.length('weeks')) + ' weeks ago' 
+  )
 })
 
 module.exports = mongoose.model("Thread", ThreadSchema)
